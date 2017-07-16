a4fee63fc5883664f0ed1b8c0125a553f14c22f9
NivenT <nachenjang@gmail.com>
2017-07-15 20:07:13 -0700

    replace specific paths with BASE_DIR

--- feed/views.py
+++ feed/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render_to_response
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 def index(request):
-	return render_to_response('/Users/charlesnj/hackathon/friktion/feed/friktion/index.html')
+	return render_to_response(BASE_DIR + '/feed/friktion/index.html')
 
